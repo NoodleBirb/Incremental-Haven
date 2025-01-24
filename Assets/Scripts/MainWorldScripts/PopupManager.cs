5fd3c5d4fd0c3969529de73d925efdae367ca3d8
NoodleBirb <ethanvolcanic@gmail.com>
2025-01-24 10:08:01 -0500

    updated popups to fit new GUI system (forgot to do that)

--- Assets/Scripts/MainWorldScripts/PopupManager.cs
+++ Assets/Scripts/MainWorldScripts/PopupManager.cs
@@ -1,75 +1,21 @@
 using System.Collections.Generic;
 using System.Data;
+using TMPro;
 using Unity;
 using Unity.VisualScripting;
 using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class PopupManager : MonoBehaviour {
-    static int count;
-    static List<Popup> popupList;
-    
-    void Start() {
-        popupList ??= new();
-    }
-
-    void OnGUI() {
-        if (popupList.Count != 0) {
-            foreach (Popup popup in popupList) {
-                GUI.Box(popup.GetGuiPos(), popup.GetName() + ": " + popup.GetText());
-                if (GUI.Button(popup.GetExitPos(), "X")) {
-                    popupList.Remove(popup);
-                    RecalculatePositions();
-                    break;
-                }
-            }
-        }
-    }
 
     public static void AddPopup(string name, string text) {
-        popupList.Add(new Popup(name, text));
-    }
+        GameObject popup = GameObject.Instantiate(Resources.Load<GameObject>("UI/Popup"));
+        popup.transform.Find("Popup Name").GetComponent<TextMeshProUGUI>().text = name + ":";
+        popup.transform.Find("Popup Text").GetComponent<TextMeshProUGUI>().text = text;
+        popup.transform.Find("Close Button").GetComponent<Button>().onClick.AddListener(() => GameObject.Destroy(popup));
+        popup.transform.SetParent(GameObject.Find("Popup Container").transform);
 
-    static void RecalculatePositions() {
-        count = 0;
-        foreach(Popup popup in popupList) {
-            popup.SetGuiPos(new(0, Screen.height - 30 - 30 * count, Screen.width / 4, 30));
-            popup.SetExitPos(new(Screen.width / 4 - 20, Screen.height - 30 - 30 * count, 20, 20));
-            count++;
-        }
-    }
-    private class Popup {
-        readonly string name;
-        readonly string text;
-        Rect guiPos;
-        Rect exitPos;
-        public Popup(string name, string text) {
-            Debug.Log("im working kind of");
-            this.name = name;
-            this.text = text;
-            guiPos = new(0, Screen.height - 30 - 30 * count, Screen.width / 4, 30);
-            exitPos = new(Screen.width / 4 - 20, Screen.height - 30 - 30 * count, 20, 20);
-            count++;
-        }
-
-        public string GetName() {
-            return name;
-        }
-        public string GetText() {
-            return text;
-        }
-        public Rect GetGuiPos() {
-            return guiPos;
-        }
-        public Rect GetExitPos() {
-            return exitPos;
-        }
-        public void SetGuiPos(Rect guiPos) {
-            this.guiPos = guiPos;
-        }
-        public void SetExitPos(Rect exitPos) {
-            this.exitPos = exitPos;
-        }
     }
 }
